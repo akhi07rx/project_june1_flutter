413661351aa05d0744cb1a8cc24f7cc635fb7266
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-07-27 17:14:27 +0530

    Initial Commit

--- lib/splash2.dart
+++ lib/splash2.dart
@@ -9,38 +9,43 @@ class Splash2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
 
-        // set background color way 1
-        // backgroundColor: Colors.black26,
+      // set background color way 1
+      // backgroundColor: Colors.black26,
 
         body: Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.bottomLeft,
-              end: Alignment.topRight,
-              colors: [Colors.blue, Colors.white30, Colors.purpleAccent])),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Icon(Icons.sailing_outlined,
-            // size: 80,
-            // color: Colors.purple,),
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/img1.jpg"))),
 
-            Image.asset(
-              "assets/icons/icon.png",
-              height: 200,
-              width: 200,
+          // gradient: LinearGradient(
+          //     begin: Alignment.bottomLeft,
+          //     end: Alignment.topRight,
+          //     colors: [Colors.blue, Colors.white30, Colors.purpleAccent])),
+
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Icon(Icons.sailing_outlined,
+                // size: 80,
+                // color: Colors.purple,),
+
+                Image.asset(
+                  "assets/icons/icon.png",
+                  height: 200,
+                  width: 200,
+                ),
+                const Text(
+                  "My Application",
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purple),
+                ),
+              ],
             ),
-            const Text(
-              "My Application",
-              style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.purple),
-            ),
-          ],
-        ),
-      ),
-    ));
+          ),
+        ));
   }
 }
