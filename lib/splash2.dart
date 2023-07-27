5b1555858b055b6d4f9126095c96985b1370358e
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-07-27 13:58:25 +0530

    Initial Commit

--- lib/splash2.dart
+++ lib/splash2.dart
@@ -8,28 +8,38 @@ class Splash2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black26,
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Icon(Icons.sailing_outlined,
-          // size: 80,
-          // color: Colors.purple,),
 
-          Image.asset(
-            "assets/icons/icon.png",
-            height: 200,
-            width: 200,
-          ),
-          const Text(
-            "My Application",
-            style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.purple),
-          ),
-        ],
+        // set background color way 1
+        // backgroundColor: Colors.black26,
+
+        body: Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+              colors: [Colors.blue, Colors.white30, Colors.purpleAccent])),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Icon(Icons.sailing_outlined,
+            // size: 80,
+            // color: Colors.purple,),
+
+            Image.asset(
+              "assets/icons/icon.png",
+              height: 200,
+              width: 200,
+            ),
+            const Text(
+              "My Application",
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.purple),
+            ),
+          ],
+        ),
       ),
     ));
   }
